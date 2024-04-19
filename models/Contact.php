24358e7ea0ee2984e8a53a788b05116777eadc6b
Elvira Vanny <116472940+newthella@users.noreply.github.com>
2024-04-19 15:11:53 +0700

    Delete models directory

--- models/Contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Manager
-{
-    public int $id;
-    public string $nomor_telepon;
-    public string $owner;
-}
--- models/Manager.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Manager
-{
-    public int $id;
-    public string $username;
-    public string $email;
-    public string $password;
-}
